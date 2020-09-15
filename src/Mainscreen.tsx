05a46b18d81078ce43626c2e475af1b6d87e74ce
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-09-15 12:49:58 -0700

    Show simple modal on pressing button

--- src/Mainscreen.tsx
+++ src/Mainscreen.tsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from './Layout';
+import Modal from 'react-modal';
 
-interface Props  {
+interface Props {
     user: string;
 }
 
-const Mainscreen: React.FC<Props> = ({user}) => {
-    return (
-        <Layout className="container-fluid" title={`Welcome back, ${user}`} description="Let's add some education details">
+const Mainscreen: React.FC<Props> = ({ user }) => {
+    const[isOpen, setIsOpen] = useState(false);
 
+    const toggleModal = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const sidePanel = () => (
+        <div className="card">
+            <h4 className="card-header">Your Education Info</h4>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    Eduaction 1
+                </li>
+            </ul>
+        </div>
+    );
+
+    return (
+        <Layout className="container-fluid" title={`Welcome back, ${user}`} description="Let's add some education details" >
+            <div className="row">
+                <div className="col-sm-12 text-center mb-5">
+                    <button className="btn btn-primary" onClick={toggleModal}>Add new education</button>
+                </div>
+                <Modal isOpen={isOpen} onRequestClose={toggleModal} contentLabel="My modal" className="mymodal" overlayClassName="myoverlay" closeTimeoutMS={500}>
+                    <div>My Modal</div>
+                    <button onClick={toggleModal}>Save</button>
+                </Modal>
+                <div className="col-sm-12 col-md-3">{sidePanel()}</div>
+                <div className="col-sm-12 col-md-9"></div>
+            </div>
         </Layout>
     )
 }
