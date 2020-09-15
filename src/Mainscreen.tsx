cdf11df04131c83303f183695b29935652560456
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-09-15 10:41:21 -0700

    Add Mainscreen component

---
+++ src/Mainscreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from './Layout';
+
+interface Props  {
+    user: string;
+}
+
+const MainScreen: React.FC<Props> = ({user}) => {
+    return (
+        <Layout className="container-fluid" title={`Welcome back ${user}`} description="Let's add some education details">
+            
+        </Layout>
+    )
+}
+
+export default MainScreen;
