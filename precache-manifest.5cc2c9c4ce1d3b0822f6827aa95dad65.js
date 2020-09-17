3c04db26d33b86662c2455f983bfa034b1512a73
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-09-17 16:20:08 -0700

    Updates

---
+++ precache-manifest.5cc2c9c4ce1d3b0822f6827aa95dad65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556d56f86b0e6a795f4a8761903db791",
+    "url": "/Showwcase-Develoer-Challenge/index.html"
+  },
+  {
+    "revision": "a9d77ccf22a5dacd9302",
+    "url": "/Showwcase-Develoer-Challenge/static/css/main.806047bd.chunk.css"
+  },
+  {
+    "revision": "021e36a58151f7d80606",
+    "url": "/Showwcase-Develoer-Challenge/static/js/2.b21ab9ab.chunk.js"
+  },
+  {
+    "revision": "f408d1cf3336fb7b0693aa7b1328cd62",
+    "url": "/Showwcase-Develoer-Challenge/static/js/2.b21ab9ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d77ccf22a5dacd9302",
+    "url": "/Showwcase-Develoer-Challenge/static/js/main.9fc9c763.chunk.js"
+  },
+  {
+    "revision": "881aecb0f27b462f4994",
+    "url": "/Showwcase-Develoer-Challenge/static/js/runtime-main.b1f16ec0.js"
+  }
+]);
